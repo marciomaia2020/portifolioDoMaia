a4e6609e8812461ec1380d03b9b3b04a7884dedd
projetospyton2025 <marciofernando.maia@yahoo.com.br>
2025-08-28 13:34:27 -0300

    Update files

--- downloads/Microsoft.PowerShell_profile.ps1
+++
@@ -1,8 +0,0 @@
-function activate{
-    param([string]$path = ".\venv\Scripts\Activate")
-    if (Test-Path $path) {
-        & $path
-    } else {
-        Write-Host "O caminho para o ambiente virtual não foi encontrado: $path" -ForegroundColor Red
-    }
-}
